c387e4317e367aca55d6154e098385f8a38f6ac5
kaletejas <tejaskale.messi@gmail.com>
2023-03-05 13:04:36 -0500

    Initial commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://127.0.0.1/books229"
+  "URI": "mongodb+srv://tejaskale:tejasmongo@cluster0.hcwfwms.mongodb.net/?retryWrites=true&w=majority"
+};
