b746bf4b5b6f9248311acd6124c6636b0608b4b8
kaletejas <tejaskale.messi@gmail.com>
2023-03-05 15:44:38 -0500

    Updated URI and delete button

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
   //"URI": "mongodb://127.0.0.1/books229"
-  "URI": "mongodb+srv://tejaskale:tejasmongo@cluster0.hcwfwms.mongodb.net/?retryWrites=true&w=majority"
+  "URI": "mongodb+srv://tejaskale:tejasmongo@cluster0.itzgoqx.mongodb.net/books229?retryWrites=true&w=majority"
 };
